2951b30afe38ef78b3b6760f138c2b899775958a
juanchos85 <86373520+juanchos85@users.noreply.github.com>
2023-12-17 01:17:43 -0300

    reusable componente Container

--- app/component/Container.tsx
+++ app/component/Container.tsx
@@ -1,6 +1,14 @@
-
-export const Container = () => {
+interface ContainerProps{
+children:React.ReactNode
+}
+export const Container:React.FC <ContainerProps> = ({children}) => {
   return (
-    <div>Container</div>
+    <div
+    className="
+    max-w-[1]
+    mx-auto
+    xl:px-20
+    mx:px-2
+    px-4">{children}</div>
   )
 }
