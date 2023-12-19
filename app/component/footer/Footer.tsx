b0d5c51af840f7d2c7907b76d36abc66284dd675
juanchos85 <86373520+juanchos85@users.noreply.github.com>
2023-12-19 15:47:34 -0300

    footer Items

--- app/component/footer/Footer.tsx
+++ app/component/footer/Footer.tsx
@@ -1,6 +1,73 @@
+import { Container } from "../Container";
+import { FooterList } from "./FooterList";
+import Link from "next/link";
 
 export const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer
+      className="
+    bg-slate-700
+    text-slate-200
+    text-sm
+    mt-16"
+    >
+      <Container>
+        <div
+          className="
+        flex
+        flex-col
+        md:flex-row
+        justify-between
+        pt-16
+        pb-8"
+        >
+          <FooterList>
+            <h3>Shop Categories</h3>
+            <Link
+              href="#"
+              className="
+          "
+            >
+              Phones
+            </Link>
+            <Link
+              href="#"
+              className="
+          "
+            >
+              Laptops
+            </Link>
+            <Link
+              href="#"
+              className="
+          "
+            >
+              Desktops
+            </Link>
+            <Link
+              href="#"
+              className="
+          "
+            >
+              Watches
+            </Link>
+            <Link
+              href="#"
+              className="
+          "
+            >
+              Tvs
+            </Link>
+            <Link
+              href="#"
+              className="
+          "
+            >
+              Accesories
+            </Link>
+          </FooterList>
+        </div>
+      </Container>
+    </footer>
+  );
+};
---
+++ app/component/footer/FooterList.tsx
@@ -0,0 +1,18 @@
+interface FooterListProp{
+    children:React.ReactNode
+}
+
+export const FooterList: React.FC <FooterListProp> = ({children}) => {
+  return (
+    <div className="
+    w-full
+    sm:w-1/2
+    md:w-1/4
+    lg:w-1/6
+    mb-6
+    flex
+    flex-col
+    gap-2">{children}</div>
+  )
+}
+
