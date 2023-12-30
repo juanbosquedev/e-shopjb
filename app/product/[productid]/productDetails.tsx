5664426cd73623e864b2b43e73b0db9f5c00e8c0
juanchos85 <86373520+juanchos85@users.noreply.github.com>
2023-12-30 15:13:36 -0300

    category, brand, dinamic color for stock

--- app/product/[productid]/productDetails.tsx
+++ app/product/[productid]/productDetails.tsx
@@ -13,17 +13,20 @@ const ProductDetails: React.FC<ProductDetailsProps> = ({ product }) => {
     <div className="grid grid-cols-1 md:grid-cols-2 gap-12">
       <div>{/* <Images></Images> */}Images</div>
       <div className="flex flex-col gap-1 text-slate-500 text-sm">
-        
         <h2 className="text-3xl font-medium text-slate-700">{product.name}</h2>
-        
+
         <div className="flex items-center gap-2">
           <Rating value={Ratings} readOnly></Rating>
           <div>{product.reviews.length} reviews</div>
         </div>
-        <Horizontal/>
-<div className="text-justify">
-    {product.description}
-</div>
+        <Horizontal />
+        <div className="text-justify">{product.description}</div>
+        <Horizontal />
+<div><span className="font-semibold">CATEGORY: </span>{product.category}</div>
+<div><span className="font-semibold">BRAND: </span>{product.brand}</div>
+<div className={product.inStock ? 'text-teal-400': 'text-rose-400'}>{product.inStock ? ' In Stock' : 'Out of Stock'}</div>
+<Horizontal />
+
       </div>
     </div>
   );
