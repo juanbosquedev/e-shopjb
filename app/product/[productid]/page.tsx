2490e805c988e340f0090ce18e2dd1a8c1d70ada
juanchos85 <86373520+juanchos85@users.noreply.github.com>
2024-01-02 23:05:04 -0300

    rating component

--- app/product/[productid]/page.tsx
+++ app/product/[productid]/page.tsx
@@ -1,13 +1,28 @@
 import { Container } from "@/app/component/Container";
-import ProductDetails from '../[productid]/productDetails'
+import ProductDetails from "../[productid]/productDetails";
 import product from "@/utils/product";
+import ListRating from "./ListRating";
 
-interface IParams{
-    productId?:string
+interface IParams {
+  productId?: string;
 }
 
-const Product = ({params}:{params:IParams}) => {
-    return <div className="p-8"><Container><ProductDetails product = {product}></ProductDetails></Container></div>;
-}
- 
+const Product = ({ params }: { params: IParams }) => {
+  return (
+    <div className="p-8">
+      <Container>
+        <ProductDetails product={product}></ProductDetails>
+        <div className="
+        flex
+        fex-col
+        mt-20
+        gap-4">
+          <div>Add Rating</div>
+          <ListRating product={product}/>
+        </div>
+      </Container>
+    </div>
+  );
+};
+
 export default Product;
