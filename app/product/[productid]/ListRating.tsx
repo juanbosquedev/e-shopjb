e945c27e98d24fd9b131138dea6e6a67c5a7463e
juanchos85 <86373520+juanchos85@users.noreply.github.com>
2024-01-02 23:21:52 -0300

    user avatar riview, ready

--- app/product/[productid]/ListRating.tsx
+++ app/product/[productid]/ListRating.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Avatar from "@/app/component/Avatar";
 import Heading from "@/app/component/products/Heading";
 import { Rating } from "@mui/material";
 import moment from "moment";
@@ -19,7 +20,7 @@ const ListRating: React.FC<ListRatingProps> = ({ product }) => {
               <div key={review.id} className="max-w-300px">
                 <div className="flex gap-2 items-center ">
 
-                <div >Avatar</div>
+                <Avatar src={review?.user.image}/>
                 <div className="font-semibold">{review?.user.name}</div>
                 <div className="font-light">{moment(review.createdDate).fromNow()}</div>
                 </div>
