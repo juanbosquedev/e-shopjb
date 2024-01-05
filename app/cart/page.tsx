dabdf007a83b3e62266ead93095048b1f10dd879
juanchos85 <86373520+juanchos85@users.noreply.github.com>
2024-01-05 20:55:13 -0300

    cart route

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
-
+import { Container } from '../component/Container';
+import CartClient from './CartClient';
 const Cart = () => {
   return (
-    <div className='pt-8'><Container></Container></div>
+    <div className='pt-8'><Container><CartClient></CartClient></Container></div>
   )
 }
 export default Cart;
