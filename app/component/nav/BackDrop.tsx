4415339cd4d9ef346bd491d4dbef16aa9cfe4a8e
juanchos85 <86373520+juanchos85@users.noreply.github.com>
2024-01-20 15:57:14 -0300

    BackDrop

---
+++ app/component/nav/BackDrop.tsx
@@ -0,0 +1,20 @@
+
+ interface BackDropProps{
+    onClick:()=>void;
+ }
+
+const BackDrop: React.FC<BackDropProps> = ({onClick}) => {
+    return <div className="
+    z-20
+    bg-slate-200
+    apacity-50
+    w-screen
+    h-screen
+    fixed
+    top-0
+    left-0"
+    onClick={onClick}></div>
+
+}
+ 
+export default BackDrop;
--- app/component/nav/UserMenu.tsx
+++ app/component/nav/UserMenu.tsx
@@ -6,6 +6,7 @@ import { AiFillCaretDown } from "react-icons/ai";
 import Link from "next/link";
 import MenuItem from "./MenuItem";
 import { signOut } from "next-auth/react";
+import BackDrop from "./BackDrop";
 
 const UserMenu = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -76,6 +77,7 @@ const UserMenu = () => {
           </div>
         )}
       </div>
+      {isOpen ? <BackDrop onClick={toggleOpen}/>:nul}
     </>
   );
 };
