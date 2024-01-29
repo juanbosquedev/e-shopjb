e7ff125c93e42e0ae0bf379a93370958fa0b63e8
juanchos85 <86373520+juanchos85@users.noreply.github.com>
2024-01-29 10:00:23 -0300

    add product route protected

--- app/admin/add-products/AddProductForm.tsx
+++ app/admin/add-products/AddProductForm.tsx
@@ -1,24 +1,6 @@
-import { getCurrentUser } from "@/actions/GetCurrentUser";
-import { Container } from "@/app/component/Container";
-import NullData from "@/app/component/NullData";
-import FormWrap from "@/app/component/products/FromWrap";
-
-const AddProductForm = async () => {
-    const currentUser = await getCurrentUser();
-     
-    if (!currentUser || currentUser.role !== "ADMIN") {
-        return <NullData title="Ooops Acces denied"/>
-    }
-    
-  return (
-    <div className="p-8">
-      <Container>
-        <FormWrap>
-          <AddProductForm />
-        </FormWrap>
-      </Container>
-    </div>
-  );
+"use client";
+const AddProductForm = () => {
+  return <div className="p-8">hola</div>;
 };
 
 export default AddProductForm;
--- app/admin/add-products/page.tsx
+++ app/admin/add-products/page.tsx
@@ -1,5 +1,26 @@
-const AddProducts = () => {
-    return (  <div>Add Products</div>);
-}
- 
+import { getCurrentUser } from "@/actions/GetCurrentUser";
+import { Container } from "@/app/component/Container";
+import NullData from "@/app/component/NullData";
+import FormWrap from "@/app/component/products/FromWrap";
+import AddProductForm from "./AddProductForm";
+
+const AddProducts = async () => {
+  const currentUser = await getCurrentUser();
+
+  if (!currentUser || currentUser.role !== "ADMIN") {
+    return <NullData title="Ooops Acces denied" />;
+  }
+
+  return (
+    <div className="p-8">
+      {currentUser.email}
+      <Container>
+        <FormWrap>
+          <AddProductForm />
+        </FormWrap>
+      </Container>
+    </div>
+  );
+};
+
 export default AddProducts;
