7eb3e5f31f44a3fa86747abae98f7c5647490d8c
juanchos85 <86373520+juanchos85@users.noreply.github.com>
2024-01-31 11:08:54 -0300

    products form

--- app/admin/add-products/AddProductForm.tsx
+++ app/admin/add-products/AddProductForm.tsx
@@ -4,6 +4,8 @@ import Heading from "@/app/component/products/Heading";
 import Input from "@/app/component/inputs/Input";
 import { useState } from "react";
 import { FieldValues, useForm } from "react-hook-form";
+import TextArea from "@/app/component/inputs/TextArea";
+import CustomCheckbox from "@/app/component/inputs/CustomCheckbox";
 
 const AddProductForm = () => {
   const [isLoading, setIsLoading] = useState(false);
@@ -36,7 +38,7 @@ const AddProductForm = () => {
         errors={errors}
         required
       />
-         <Input
+      <Input
         id="price"
         label="Price"
         disabled={isLoading}
@@ -45,7 +47,7 @@ const AddProductForm = () => {
         type="number"
         required
       />
-         <Input
+      <Input
         id="brand"
         label="Brand"
         disabled={isLoading}
@@ -53,6 +55,23 @@ const AddProductForm = () => {
         errors={errors}
         required
       />
+      <TextArea
+        id="description"
+        label="Description"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+      />
+      <CustomCheckbox
+        id="inStock"
+        register={register}
+        label="This product is in stock"
+      />
+      <div className="w-full font-medium">
+        <div className="mb-2 font-semibold">Select a Category</div>
+      <div className="grid grid-cols-2 md:grid-cols-3 max-h[50vh] overflow-y-auto"></div>
+      </div>
     </>
   );
 };
---
+++ utils/Categories.ts
@@ -0,0 +1,11 @@
+import { AiFillPhone, AiOutlineDesktop, AiOutlineLaptop } from "react-icons/ai";
+import { MdStorefront, MdTv, MdWatch } from "react-icons/md";
+
+export const categories = [
+  { label: "All", icon: MdStorefront },
+  { label: "Phone", icon: AiFillPhone },
+  { label: "Laptop", icon: AiOutlineLaptop },
+  { label: "Descktop", icon: AiOutlineDesktop },
+  { label: "Watch", icon: MdWatch },
+  { label: "Tv", icon: MdTv },
+];
